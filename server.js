88814874b2f2151e9f09859f036eb544f2a956d1
Bowen Revill <bowen.revill@vivint.com>
2025-01-24 17:22:37 -0700

    redirect logic

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.get('/', (req, res) => {
 app.use('/api/players', playersRouter)
 app.use('/api/results', resultsRouter)
 
+app.use((req, res, next) => {
+  const host = req.headers.host;
+  const isOldUrl = host === 'cdlwordle-production.up.railway.app';
+
+  if (isOldUrl) {
+    const newUrl = `https://www.cdlwordle.me${req.originalUrl}`;
+    return res.redirect(301, newUrl); // 301 indicates a permanent redirect
+  }
+  
+  next();
+});
+
 mongoose.connect(process.env.DB_STRING).then(() => {
   console.log("connected to DB")
   app.listen(port, () => {
